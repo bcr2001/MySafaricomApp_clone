2ecb64f9c814068c386a2f91887293935b1ca759
brian24 <bcr9987@gmail.com>
2023-05-25 21:18:25 +0300

    created a widget that handles the construction of the elevated button instead of individually building each one of them

--- lib/main.dart
+++ lib/main.dart
@@ -31,18 +31,59 @@ class _MainAppHandler extends State<MyMainApp> {
   Color _initialHomeColor = const Color(0xFF0AE500);
 
   Widget _buildNavButtons(
-      int index, String imageLocation, double imageHeight, double imageWidth) {
+      int index,
+      String imageLocation,
+      double imageHeight,
+      double imageWidth,
+      Color initialColor,
+      String buttonName,
+      double textPaddingTop,
+      [double textPaddingBottom = 0]) {
     return ElevatedButton(
-        onPressed: () {},
+        style: ButtonStyle(
+          elevation: MaterialStateProperty.all(0),
+          backgroundColor: MaterialStateProperty.all(Colors.transparent),
+        ),
+        onPressed: () {
+          setState(() {
+            // Reset all colors
+            _initialMpesaColor = Colors.white;
+            _initialHomeColor = Colors.white;
+            _initialDiscoverColor = Colors.white;
+            _initialAccountColor = Colors.white;
+
+            // Only change the color for the selected index
+            if (index == 0) {
+              _currentIndex = 0;
+              _initialHomeColor = const Color(0xFF0AE500);
+            } else if (index == 1) {
+              _currentIndex = 1;
+              _initialMpesaColor = const Color(0xFF0AE500);
+            } else if (index == 2) {
+              _currentIndex = 2;
+              _initialDiscoverColor = const Color(0xFF0AE500);
+            } else {
+              _currentIndex = 3;
+              _initialAccountColor = const Color(0xFF0AE500);
+            }
+          });
+        },
         child: Column(
-          mainAxisSize: MainAxisSize.min,
+          mainAxisSize: MainAxisSize.max,
           children: [
             Image.asset(
               imageLocation,
               fit: BoxFit.contain,
               height: imageHeight,
               width: imageWidth,
-              color: _initialHomeColor,
+              color: initialColor,
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: textPaddingTop, bottom: textPaddingBottom),
+              child: Text(
+                buttonName,
+                style: TextStyle(fontSize: 12, color: initialColor),
+              ),
             )
           ],
         ));
@@ -72,10 +113,10 @@ class _MainAppHandler extends State<MyMainApp> {
         floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
         floatingActionButton: Padding(
             padding: const EdgeInsets.only(
-              top: 30,
+              top: 30
             ),
             child: Container(
-              margin: const EdgeInsets.only(right: 16),
+              margin: const EdgeInsets.only(left: 15, right: 30),
               width: 65,
               height: 65,
               child: FloatingActionButton.large(
@@ -98,43 +139,29 @@ class _MainAppHandler extends State<MyMainApp> {
           color: const Color(0xFF2E313E),
           child: Row(
             // main row that holds the entire bottom navigation elements
-            mainAxisSize: MainAxisSize.min,
+            mainAxisSize: MainAxisSize.max,
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
+              //first row for the home and mpesa routes
               Row(
-                //first row for the home and mpesa routes
-                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  // home button
-                  ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          _currentIndex = 0;
-                          _initialHomeColor = const Color(0xFF0AE500);
-                          _initialAccountColor = Colors.white;
-                          _initialDiscoverColor = Colors.white;
-                          _initialMpesaColor = Colors.white;
-                        });
-                      },
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Image.asset(
-                            "assets/images/saf_icons/homePage.png",
-                            fit: BoxFit.contain,
-                            height: 25,
-                            width: 25,
-                            color: _initialHomeColor,
-                          ),
-                          Padding(
-                              padding: const EdgeInsets.only(top: 1),
-                              child: Text(
-                                "Home",
-                                style: TextStyle(
-                                    fontSize: 12, color: _initialHomeColor),
-                              ))
-                        ],
-                      )),
+                  // home route elevated button
+                  _buildNavButtons(0, "assets/images/saf_icons/homePage.png",
+                      25, 25, _initialHomeColor, "Home", 4),
+
+                  //  mpesa route elevated button
+                  _buildNavButtons(1, "assets/images/saf_icons/mpesaHome.png",
+                      19, 30, _initialMpesaColor, "MPESA", 9)
+                ],
+              ),
+              // second row for the discover and account routes
+              Row(
+                children: [
+                  // discover route 
+                  _buildNavButtons(2, "assets/images/saf_icons/discover_icon.png", 27, 35, _initialDiscoverColor, "Discover", 0, 2),
+
+                  // account route
+                  _buildNavButtons(3, "assets/images/saf_icons/account_icon.png", 27, 35, _initialAccountColor, "Account", 1)
                 ],
               )
             ],
@@ -148,4 +175,4 @@ class _MainAppHandler extends State<MyMainApp> {
 // colors am using
 // 07A400
 // 0AE500
-// 2E313E
+// 2E313

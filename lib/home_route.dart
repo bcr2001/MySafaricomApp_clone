9306b8e172d318efb6a0d2b8c91fc4233dc54260
brian24 <bcr9987@gmail.com>
2023-05-28 08:00:40 +0300

    built the sliver app bar action buttons and the pop up menu alert dialog

--- lib/home_route.dart
+++ lib/home_route.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
+import 'my_action_buttons.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class MyHomeRoute extends StatefulWidget {
@@ -11,27 +11,41 @@ class MyHomeRoute extends StatefulWidget {
 
 class _HomeRouteHandler extends State<MyHomeRoute> {
   // ignore: prefer_final_fields
-  // handles the page change of the pageview
-  PageController _pageController = PageController();
+  // handles the page change of the pageview(promotion)
+  final PageController _pageControllerPromotion = PageController();
 
-  // first widget in the page view
-  double _currentPage = 0;
+  // handles the page change of the pageview(promotion)
+  final PageController _pageControllerForYou = PageController();
 
+  // first widget in the page view(promotion)
+  double _currentPagePromotion = 0;
+
+  // first widget in the page view(for you)
+  double _currentPageForYou = 0;
+
+  @override
   void initState() {
     super.initState();
-    _pageController.addListener(() {
+    _pageControllerPromotion.addListener(() {
       setState(() {
-        _currentPage = _pageController.page!;
+        _currentPagePromotion = _pageControllerPromotion.page!;
+      });
+    });
+    _pageControllerForYou.addListener(() {
+      setState(() {
+        _currentPagePromotion = _pageControllerForYou.page!;
       });
     });
   }
 
+  @override
   void dispose() {
     super.dispose();
-    _pageController.dispose();
+    _pageControllerPromotion.dispose();
+    _pageControllerForYou.dispose();
   }
 
-  // list of widgets that will go inside the page view
+  // list of widgets that will go inside the page view (promotion)
   List<String> pageViewElemets = [
     "assets/images/saf_components/slide_get_extra.png",
     "assets/images/saf_components/slide_get_maps.png",
@@ -39,29 +53,52 @@ class _HomeRouteHandler extends State<MyHomeRoute> {
     "assets/images/saf_components/slide_bonga.png",
   ];
 
+  // list of widgets for the For You section
+  List<String> forYouPageView = [
+    "assets/images/saf_components/for_you_monthly.png",
+    "assets/images/saf_components/for_you_tips.png",
+    "assets/images/saf_components/for_you_wifi.png",
+    "assets/images/saf_components/for_you_baze.png",
+  ];
+
   // takes the pageViewElements and puts them in a pageView
-  Widget pageViewConstructor({required List<String> pageViewElements}) {
+  Widget pageViewConstructor(
+      {required currentPageController,
+      required List<String> pageViewElements,
+      required double containerHeight,
+      required double containerWidth}) {
     return Column(children: [
       // pageView Builder
       Container(
-          width: 180,
-          height: 160,
-          child: PageView.builder(
-              controller: _pageController,
-              itemCount: pageViewElements.length,
-              itemBuilder: (context, index) {
-                return Image.asset(
-                  pageViewElements[index],
-                  height: 300,
-                  fit: BoxFit.fill,
-                );
-              })),
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(30)),
+          width: containerWidth,
+          height: containerHeight,
+          child: ClipRRect(
+              borderRadius: BorderRadius.circular(5),
+              child: PageView.builder(
+                  controller: currentPageController,
+                  itemCount: pageViewElements.length,
+                  itemBuilder: (context, index) {
+                    return ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          padding: EdgeInsets.zero,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10))),
+                      onPressed: () {
+                        print("${pageViewElemets[index]} was clicked");
+                      },
+                      child: Image.asset(
+                        pageViewElements[index],
+                        fit: BoxFit.fill,
+                      ),
+                    );
+                  }))),
       //smoothPageIndicator
       Padding(
           padding: const EdgeInsets.only(top: 5, bottom: 5),
           child: SmoothPageIndicator(
-            controller: _pageController,
-            count: 4,
+            controller: currentPageController,
+            count: pageViewElemets.length,
             effect: const WormEffect(
                 dotColor: Color(0xFF2E313E),
                 activeDotColor: Color(0xFFFF0000),
@@ -116,38 +153,33 @@ class _HomeRouteHandler extends State<MyHomeRoute> {
   }
 
   // hot deals SingleChildScrollView widget
-  Widget hotDealsConstructor(
-      {required double hotDealHeight, required double hotDealWidth}) {
+  Widget hotDealsConstructor() {
     return SingleChildScrollView(
         scrollDirection: Axis.horizontal,
         child: Padding(
-          padding: const EdgeInsets.only(left: 5),
+          padding: const EdgeInsets.only(left: 10),
           child: Row(
             children: [
-              Image.asset(
-                "assets/images/saf_components/hot_deal_1.png",
-                fit: BoxFit.contain,
-                height: hotDealHeight,
-                width: hotDealWidth,
-              ),
-              Image.asset(
-                "assets/images/saf_components/hot_deal_2.png",
-                fit: BoxFit.contain,
-                height: hotDealHeight,
-                width: hotDealWidth,
-              ),
-              Image.asset(
-                "assets/images/saf_components/hot_deal_3.png",
-                fit: BoxFit.contain,
-                height: hotDealHeight,
-                width: hotDealWidth,
-              ),
-              Image.asset(
-                "assets/images/saf_components/hot_deal_4.png",
-                fit: BoxFit.contain,
-                height: hotDealHeight,
-                width: hotDealWidth,
-              ),
+              hotDealsButton(
+                  hotDealsImage: "hot_deal_1.png",
+                  onPressedFunction: () {
+                    print("hot_deal_1");
+                  }),
+              hotDealsButton(
+                  hotDealsImage: "hot_deal_2.png",
+                  onPressedFunction: () {
+                    print("hot_deal_2");
+                  }),
+              hotDealsButton(
+                  hotDealsImage: "hot_deal_3.png",
+                  onPressedFunction: () {
+                    print("hot_deal_3");
+                  }),
+              hotDealsButton(
+                  hotDealsImage: "hot_deal_4.png",
+                  onPressedFunction: () {
+                    print("hot_deal_4");
+                  }),
             ],
           ),
         ));
@@ -158,7 +190,11 @@ class _HomeRouteHandler extends State<MyHomeRoute> {
     return CustomScrollView(
       slivers: [
         SliverAppBar(
-          actions: [],
+          actions: [
+            Builder(builder: (BuildContext context) {
+              return actionButtons(context);
+            })
+          ],
           floating: true,
           pinned: true,
           expandedHeight: 180,
@@ -211,7 +247,10 @@ class _HomeRouteHandler extends State<MyHomeRoute> {
                         children: [
                           // pageView
                           pageViewConstructor(
-                              pageViewElements: pageViewElemets),
+                              currentPageController: _pageControllerPromotion,
+                              pageViewElements: pageViewElemets,
+                              containerHeight: 160,
+                              containerWidth: 180),
 
                           // data and calls service
                           serviceButtonBuilder(
@@ -304,14 +343,52 @@ class _HomeRouteHandler extends State<MyHomeRoute> {
             padding: EdgeInsets.only(bottom: 10, left: 15, top: 10),
             child: Text(
               "Hot Deals",
-              style: TextStyle(fontSize: 16),
+              style: TextStyle(fontSize: 20),
             ),
           ),
+          hotDealsConstructor(),
 
-          hotDealsConstructor(hotDealHeight: 100, hotDealWidth: 210)
           // For you
+          const Padding(
+            padding: EdgeInsets.only(bottom: 5, left: 15, top: 15),
+            child: Text(
+              "For You",
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 15, right: 15),
+            child: pageViewConstructor(
+                currentPageController: _pageControllerForYou,
+                pageViewElements: forYouPageView,
+                containerHeight: 150,
+                containerWidth: 410),
+          )
         ]))
       ],
     );
   }
+}
+
+// handles the hot deals creation and function operation
+Widget hotDealsButton(
+    {required String hotDealsImage, required VoidCallback onPressedFunction}) {
+  return Padding(
+    padding: const EdgeInsets.only(right: 10),
+    child: ClipRRect(
+      borderRadius: BorderRadius.circular(6),
+      child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+              padding: EdgeInsets.zero,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20))),
+          onPressed: onPressedFunction,
+          child: Image.asset(
+            "assets/images/saf_components/$hotDealsImage",
+            height: 120,
+            width: 220,
+            fit: BoxFit.cover,
+          )),
+    ),
+  );
 }
---
+++ lib/my_action_buttons.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+// the action buttons found on each bottom
+// navigation routes
+
+Widget actionButtons(context) {
+  return Row(
+    mainAxisSize: MainAxisSize.min,
+    children: [
+      // search icon
+      IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+
+      // notification icon
+      IconButton(onPressed: () {}, icon: const Icon(Icons.notifications)),
+
+      // pop up menu button
+      PopupMenuButton(onSelected: (value) {
+        if (value == "logout") {
+          showPopUpDialog(context);
+        }
+      }, itemBuilder: (BuildContext context) {
+        return [
+          const PopupMenuItem(
+              value: "logout",
+              child: Text(
+                "Logout",
+                style: TextStyle(fontSize: 15),
+              ))
+        ];
+      })
+    ],
+  );
+}
+
+// Notification route
+
+// alert dialog (pop up menu)
+showPopUpDialog(context) {
+  return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+          return AlertDialog(
+            title: const Text(
+              "Logout",
+              style: TextStyle(color: Color(0xFF0AE500)),
+            ),
+            content: Container(
+              width: constraints.maxWidth,
+              height: 100,
+              padding: const EdgeInsets.only(right: 5, left: 5),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Divider(),
+                  const Text("Are you sure you want to logout?"),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: const Color(0xFF2E313E)),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: const Text(
+                            "Cancel",
+                            style: TextStyle(color: Colors.white),
+                          )),
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: const Color(0xFF0AE500)),
+                          onPressed: () {},
+                          child: const Text(
+                            "LOGOUT",
+                            style: TextStyle(color: Colors.black),
+                          ))
+                    ],
+                  )
+                ],
+              ),
+            ),
+          );
+        });
+      });
+}
